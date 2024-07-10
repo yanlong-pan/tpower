e59c15463a50af909813465b54dc3bdb39eb5442
pyl <a522723936@gmail.com>
2024-07-10 13:42:42 +0800

    extract lines with MeterValues from log files

---
+++ scripts/extractor.py
@@ -0,0 +1,30 @@
+import os
+
+from utilities import loggers
+
+# Get the current script directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+def extract_meter_values():
+    raw_logs_dir_path = os.path.join(os.path.dirname(current_dir), 'statics/logs/raw')
+    raw_log_filename = input(f"Please enter the log file name under '{raw_logs_dir_path}' (eg. log1.log): ")
+    raw_log_filepath = os.path.join(raw_logs_dir_path, raw_log_filename)
+    keyword = 'MeterValues'
+
+    # Generate the relative path for the output file
+    output_path = os.path.join(os.path.dirname(current_dir), 'statics/logs/extracted', f'meter_values_from_{raw_log_filename}')
+
+    try:
+        with open(raw_log_filepath, 'r') as file, open(output_path, 'w') as output_file:
+            for line in file:
+                if keyword in line:
+                    output_file.write(line)
+        loggers.debug_file_logger.debug(f'Extracted content has been saved to: {output_path}')
+    except FileNotFoundError:
+        loggers.error_file_logger.error(f'Error: File {raw_log_filepath} not found.')
+    except Exception as e:
+        loggers.error_file_logger.error(f'An error occurred: {e}')
+
+if __name__ == "__main__":
+    # Run the extraction function
+    extract_meter_values()
