a654cb070351d4fc6ee1b77f0213ee97e032d584
pyl <a522723936@gmail.com>
2024-07-10 17:03:29 +0800

    enhance function response

--- scripts/extractor.py
+++ scripts/extractor.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from utilities import loggers
@@ -9,24 +10,47 @@ def extract_meter_values():
     raw_logs_dir_path = os.path.join(os.path.dirname(current_dir), 'statics/logs/raw')
     raw_log_filename = input(f"Please enter the log file name under '{raw_logs_dir_path}' (eg. log1.log): ")
     raw_log_filepath = os.path.join(raw_logs_dir_path, raw_log_filename)
-    keyword = 'MeterValues'
+    keyword = input("Please enter the keyword to search for (default is 'MeterValues'): ") or "MeterValues"
 
     # Generate the absolute path for the output file
     output_path = os.path.join(os.path.dirname(current_dir), 'statics/logs/extracted', f'meter_values_from_{raw_log_filename}')
 
     try:
-        extracted_lines = 0
+        extracted_lines, total_lines = 0, 0
         with open(raw_log_filepath, 'r') as file, open(output_path, 'w') as output_file:
             for line in file:
+                total_lines += 1
                 if keyword in line:
                     output_file.write(line)
                     extracted_lines += 1
-        loggers.debug_file_logger.debug(f'Extracted content ({extracted_lines} lines) has been saved to: {output_path}')
+        r = {
+            'success': True,
+            'data': {
+                'keyword': keyword,
+                'extracted_lines': extracted_lines,
+                'total_lines': total_lines,
+                'input_filepath': raw_log_filepath,
+                'output_filepath': output_path,
+            }
+        }
+        loggers.debug_file_logger.debug(json.dumps(r, indent=4))
+        return r
     except FileNotFoundError:
-        loggers.error_file_logger.error(f'Error: File {raw_log_filepath} not found.')
+        r = {
+            'success': False,
+            'error': f'Error: File {raw_log_filepath} not found.'
+        }
+        loggers.error_file_logger.error(json.dumps(r, indent=4))
+        return r
     except Exception as e:
-        loggers.error_file_logger.error(f'An error occurred: {e}')
+        r = {
+            'success': False,
+            'error': f'An error occurred: {e}'
+        }
+        loggers.error_file_logger.error(json.dumps(r, indent=4))
+        return r
 
 if __name__ == "__main__":
     # Run the extraction function
-    extract_meter_values()
+    r = extract_meter_values()
+    print(json.dumps(r, indent=4))
