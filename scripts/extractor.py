67fc6b3cbbe65d9eb9d5680862115d6c5d19256d
pyl <a522723936@gmail.com>
2024-07-10 21:31:15 +0800

    allow multiple patterns

--- scripts/extractor.py
+++ scripts/extractor.py
@@ -2,6 +2,7 @@ import json
 import os
 import re
 
+from scripts import patterns
 from utilities import loggers
 
 # Get the current script directory path
@@ -18,31 +19,43 @@ def save_keywords(keywords, file_path):
         for keyword in sorted(keywords):
             file.write(f"{keyword}\n")
 
-def extract_keywords_from_log(log_file_path, known_keywords_path, unknown_keywords_path):
-    keyword_pattern = re.compile(r'receive message \[\s*\d+\s*,\s*"[^"]+"\s*,\s*"([^"]+)"')
-    unknown_keyword_pattern = re.compile(r'receive message \[\s*\d+\s*,\s*"[^"]+"\s*,(.+)')
+def extract_keywords_from_log(log_file_path, known_keywords_path, suspicious_keywords_path):
+    keyword_patterns = [patterns.THIRD_ARRAY_ITEM_IN_QUOTES]
+    suspicious_keyword_patterns = [patterns.THIRD_ARRAY_ITEM]
     
     known_keywords = load_keywords(known_keywords_path)
-    unknown_keywords = load_keywords(unknown_keywords_path)
+    suspicious_keywords = load_keywords(suspicious_keywords_path)
     
     try:
         with open(log_file_path, 'r') as log_file:
-            for line in log_file:
+            for line_num, line in enumerate(log_file, start=1):
                 if 'receive message' in line:
-                    match = keyword_pattern.search(line)
-                    if match:
-                        keyword = match.group(1)
-                        if keyword.isalpha():  # Check if the keyword is a word (contains only letters)
-                            if keyword not in known_keywords:
-                                known_keywords.add(keyword)
-                    else:
-                        match = unknown_keyword_pattern.search(line)
-                        unknown_keyword = match.group(1)
-                        if unknown_keyword and unknown_keyword not in unknown_keywords:
-                            unknown_keywords.add(unknown_keyword)
-                        # unknown_keywords.add(line)
+                    found_keyword = False
+                    # walk through all known keyword patterns
+                    for keyword_pattern in keyword_patterns:
+                        match = keyword_pattern.search(line)
+                        if match:
+                            keyword = match.group(1)
+                            if keyword.isalpha():  # Check if the keyword is a word (contains only letters)
+                                if keyword not in known_keywords:
+                                    known_keywords.add(keyword)
+                                    loggers.debug_file_logger.debug(f'Found a new keyword \'{keyword}\' in line {line_num} within {log_file_path}')
+                                # TODO: save the current line data into the corresponding keyword file
+                            # stop propagating once found a match
+                            found_keyword = True
+                            break
+                    # walk through all suspicious keyword patterns if no known keyword is found
+                    if not found_keyword:      
+                        for suspicious_keyword_pattern in suspicious_keyword_patterns:
+                            match = suspicious_keyword_pattern.search(line)
+                            if match:
+                                suspicious_keyword = match.group(1)
+                                if suspicious_keyword and suspicious_keyword not in suspicious_keywords:
+                                    suspicious_keywords.add(suspicious_keyword)
+                                    loggers.debug_file_logger.debug(f'Found a new suspicious keyword \'{suspicious_keyword}\' in line {line_num} within {log_file_path}')
+                                # stop propagating once found a match
+                                break
         
-        return known_keywords
     except FileNotFoundError:
         print(f"Error: File {log_file_path} not found.")
         return None
@@ -51,7 +64,7 @@ def extract_keywords_from_log(log_file_path, known_keywords_path, unknown_keywor
         return None
     finally:
         save_keywords(known_keywords, known_keywords_path)
-        save_keywords(unknown_keywords, unknown_keywords_path)
+        save_keywords(suspicious_keywords, suspicious_keywords_path)
 
 
 def extract_meter_values():
@@ -105,7 +118,7 @@ if __name__ == "__main__":
     # r = extract_meter_values()
     # print(json.dumps(r, indent=4))
     extract_keywords_from_log(
-        log_file_path='/Users/panyanlong/workspace/tpower/statics/logs/raw/log2.log',
+        log_file_path='/Users/panyanlong/workspace/tpower/statics/logs/raw/log1.log',
         known_keywords_path='/Users/panyanlong/workspace/tpower/statics/keywords.txt',
-        unknown_keywords_path='/Users/panyanlong/workspace/tpower/statics/keywords_unknown.txt'
+        suspicious_keywords_path='/Users/panyanlong/workspace/tpower/statics/keywords_suspicious.txt'
     )
---
+++ scripts/patterns.py
@@ -0,0 +1,4 @@
+import re
+
+THIRD_ARRAY_ITEM=re.compile(r'receive message \[\s*\d+\s*,\s*"[^"]+"\s*,(.+)\]')
+THIRD_ARRAY_ITEM_IN_QUOTES=re.compile(r'receive message \[\s*\d+\s*,\s*"[^"]+"\s*,\s*"([^"]+)"')
