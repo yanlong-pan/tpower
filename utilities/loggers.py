e9ee52a5345eeec59483f94cc70ab8dd6371d12d
pyl <a522723936@gmail.com>
2024-07-17 11:44:18 +0800

    add functions to mute/unmute a logger

--- utilities/loggers.py
+++ utilities/loggers.py
@@ -1,5 +1,6 @@
 import logging
 from kuai_log import get_logger
+from kuai_log.logger import KuaiLogger
 
 debug_file_logger = get_logger(name='debug_file_logger', level=logging.DEBUG, log_filename='debug.log',
                 log_path='statics/logs/app/debug', is_add_file_handler=True,
@@ -10,3 +11,13 @@ error_file_logger = get_logger(name='error_file_logger', level=logging.ERROR, lo
                 log_path='statics/logs/app/error', is_add_file_handler=True,
                 json_log_path='statics/logs/app/error/json', is_add_json_file_handler=True,
             )
+
+def mute_logger(logger: KuaiLogger):
+    logger._is_add_stream_handler = False
+    logger._is_add_file_handler = False
+    logger._is_add_json_file_handler = False
+
+def unmute_logger(logger: KuaiLogger):
+    logger._is_add_stream_handler = True
+    logger._is_add_file_handler = True
+    logger._is_add_json_file_handler = True
