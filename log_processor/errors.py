925608a382d1a21a762c5db163f3ff52c6966e0f
pyl <a522723936@gmail.com>
2024-07-17 12:25:22 +0800

    custom errors

---
+++ log_processor/errors.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class ErrorMessage(Enum):
+    UNSUPPORTED_INPUT_FORMAT = "Unsupported input format"
+    UNSUPPORTED_CHARGER_SENT_REQUEST_TYPE = "Unsupported charger sent request type"
+    UNHANDLED_EXCEPTION = "Unhandled exception"
+    INTERNAL_SERVER_ERROR = "Internal server error"
+
+class CurrentlyUnSupported(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(message)
