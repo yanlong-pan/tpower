4e0e5665afc5798ec28b4c3a0bbbf2547a1ff4b8
pyl <a522723936@gmail.com>
2024-07-28 12:27:37 +0800

    split into two steps

--- log_processor/parser.py
+++ log_processor/parser.py
@@ -48,128 +48,123 @@ def add_charger_number_and_raw_data_to_content(data: ParserStepIO) -> ParserStep
     return ParserStepIO(**data_dict)
 
 def flatten_meter_value(data: ParserStepIO) -> ParserStepIO:
-    def _same_type_sample_values(to_compare: dict, values: List[dict], ignores: List[str] = ["value", "phase"]) -> List[dict]:
+    # Extract basic information
+    prev_content = data.content[0]
+    meter_values = prev_content["meterValue"]
+    prev_content.pop("meterValue", None)
+    # Collect all sampled values and add missing fields
+    all_sampled_values = []
+    for meter_value in meter_values:
+        timestamp = meter_value["timestamp"]
+        for sampled_value in meter_value["sampledValue"]:
+            if sampled_value.get('measurand') == 'Voltage' and not sampled_value.get('unit'):
+                sampled_value['unit'] = 'V'
+            all_sampled_values.append({
+                "timestamp": timestamp,
+                **sampled_value
+            })
+
+    r = ParserStepIO(content=[{
+        "all_sampled_values": all_sampled_values,
+        **prev_content
+    }], request_type=data.request_type)
+    
+    return r
+
+def process_sampled_values(data: ParserStepIO) -> ParserStepIO:
+    def _same_type_sample_values(to_compare: dict, values: List[dict], ignores: List[str] = ["value", "phase", "timestamp"]) -> List[dict]:
         return [v for v in values if comparator.shallow_compare_two_dicts(v, to_compare, ignores)]
 
     # Extract basic information
-    content = data.content[0]
-    connector_id = content["connectorId"]
-    transaction_id = content["transactionId"]
-    meter_values = content["meterValue"]
+    prev_content = data.content[0]
+    all_sampled_values = prev_content["all_sampled_values"]
 
+    # Define phases and units
     valid_units = {'V', 'Wh', 'W', 'A'}
     l_phases = {"L1", "L2", "L3"}
-    l_n_phases = {"L1-N", "L2-N", "L3-N"}
-    l_l_phases = {"L1-L2", "L2-L3", "L3-L1"}
-    all_phases = l_phases.union(l_n_phases).union(l_l_phases)
-    valid_phases = {
-        frozenset({"L1",}),
-        frozenset({"L2",}),
-        frozenset({"L3",}),
-        frozenset(l_phases),
-        frozenset(l_n_phases),
-        frozenset(l_l_phases),
-        frozenset(l_phases.union(l_n_phases)),
-        frozenset(l_phases.union(l_l_phases)),
-        frozenset(l_n_phases.union(l_l_phases)),
-        frozenset(all_phases),
-    }
+    all_phases = l_phases.union({None, "L1-N", "L2-N", "L3-N", "L1-L2", "L2-L3", "L3-L1"})
 
     # Initialize result dictionary
     merged_result = {
-        "charger_number": content['charger_number'],
-        "connector_id": connector_id,
-        "transaction_id": transaction_id,
-        "raw_data": content['raw_data'],
+        "charger_number": prev_content['charger_number'],
+        "connector_id": prev_content["connectorId"],
+        "transaction_id": prev_content["transactionId"],
+        "raw_data": prev_content['raw_data'],
     }
     for phase in l_phases:
         merged_result[phase] = {unit: [] for unit in valid_units}
 
-    # Iterate through each meterValue
-    for meter_value in meter_values:
-        timestamp = meter_value["timestamp"]
+    # Initialize dictionaries to store phases for each unit
+    phases_dict = {unit: set() for unit in valid_units}
 
-        # Initialize dictionaries to store phases and target_phases for each unit
-        phases_dict = {unit: set() for unit in valid_units}
-        target_phases_dict = {unit: set() for unit in valid_units}
-
-        # Populate phases and target_phases for each unit
-        for sampled_value in meter_value["sampledValue"]:
-            unit = sampled_value.get("unit")
+    # Populate phases to process for each valid unit
+    for sampled_value in all_sampled_values:
+        unit = sampled_value.get("unit")
+        if unit in valid_units:
             phase = sampled_value.get("phase")
-            if not phase or phase in all_phases:
+            if phase in all_phases:
                 phases_dict[unit].add(phase)
 
-        for unit, phases in phases_dict.items():
-            if not phases - {None}:
-                target_phases_dict[unit] = {None}
-            elif None in phases or frozenset(phases) in valid_phases:
-                target_phases_dict[unit] = phases
-            else:
-                raise ValidationError(f"Invalid phase configuration in sampled values for unit {unit}, which only contains data for phases: {phases}")
-
-        # Initialize a dictionary to store values for each phase
-        phase_values = phase_values = {phase: {unit: [] for unit in valid_units} for phase in l_phases}
+    # Initialize a dictionary to store values for each phase
+    phase_unit_sampled_values = {phase: {unit: [] for unit in valid_units} for phase in l_phases}
         
-        # Iterate through each sampledValue
-        for sampled_value in meter_value["sampledValue"]:
-            value = float(sampled_value["value"])
-            phase = sampled_value.get("phase")
-            unit = sampled_value.get("unit")
-            sampled_value_with_timestamp = {
-                "timestamp": timestamp,
-                "value": value,
-                "context": sampled_value.get("context"),
-                "format": sampled_value.get("format"),
-                "measurand": sampled_value.get("measurand"),
-                "phase": phase,
-                "location": sampled_value.get("location"),
-                "unit": unit,
-            }
+    # Categorize sampledValues
+    for sampled_value in all_sampled_values:
+        value = float(sampled_value["value"])
+        phase = sampled_value.get("phase")
+        unit = sampled_value.get("unit")
 
-            if unit in valid_units and phase in target_phases_dict[unit] and value > 0:
-                # When phase is absent, the measured value is interpreted as an overall value. Default to L1
-                phase_values[phase[:2] if phase else "L1"][unit].append(sampled_value_with_timestamp)
+        if unit in valid_units and phase in phases_dict[unit] and value > 0:
+            # When phase is absent, the measured value is interpreted as an overall value. Default to L1
+            phase_unit_sampled_values[phase[:2] if phase else "L1"][unit].append(sampled_value)
 
-        for unit, d in phase_values["L1"].items():
-            for phase_value in d:
-                # If there is an overall item
-                if not phase_value.get("phase"):
-                    # L1 retains the overall item
-                    merged_result["L1"][unit].append(phase_value)
-                # If it is not an overall item and there is no same type item, check whether it is single-phase or three-phase
-                elif len(_same_type_sample_values(phase_value, merged_result["L1"][unit])) == 0:
-                    # If both of the next two phases have values, it is three-phase
-                    l2_same_type_sample_values = _same_type_sample_values(phase_value, phase_values["L2"][unit])
-                    l3_same_type_sample_values = _same_type_sample_values(phase_value, phase_values["L3"][unit])
-                    if len(l2_same_type_sample_values) > 0 and len(l3_same_type_sample_values) > 0:
-                        merged_result["L1"][unit].append(phase_value)
-                        merged_result["L2"][unit].extend(l2_same_type_sample_values)
-                        merged_result["L3"][unit].extend(l3_same_type_sample_values)
-                    # If both of the next two phases are empty, it is single-phase
-                    elif len(l2_same_type_sample_values) == 0 and len(l3_same_type_sample_values) == 0:
-                        merged_result["L1"][unit].append(phase_value)
-                    else:
-                        raise ValidationError(f"Invalid data format: {phase_value['measurand']} should either be a single-phase or triphase")
+    # Process L1 sampled values
+    for unit, d in phase_unit_sampled_values["L1"].items():
+        for stored_sampled_value in d:
+            # If there is an overall item
+            if not stored_sampled_value.get("phase"):
+                # L1 retains the overall item
+                merged_result["L1"][unit].append(stored_sampled_value)
+            # If it is not an overall item and there is no same type item, check whether it is single-phase or three-phase
+            elif len(_same_type_sample_values(stored_sampled_value, merged_result["L1"][unit])) == 0:
+                # If both of the next two phases have values, it is three-phase
+                l2_same_type_sample_values = _same_type_sample_values(stored_sampled_value, phase_unit_sampled_values["L2"][unit])
+                l3_same_type_sample_values = _same_type_sample_values(stored_sampled_value, phase_unit_sampled_values["L3"][unit])
+                if len(l2_same_type_sample_values) > 0 and len(l3_same_type_sample_values) > 0:
+                    merged_result["L1"][unit].append(stored_sampled_value)
+                    merged_result["L2"][unit].extend(l2_same_type_sample_values)
+                    merged_result["L3"][unit].extend(l3_same_type_sample_values)
+                # If both of the next two phases are empty, it is single-phase
+                elif len(l2_same_type_sample_values) == 0 and len(l3_same_type_sample_values) == 0:
+                    merged_result["L1"][unit].append(stored_sampled_value)
+                else:
+                    raise ValidationError(f"Invalid data format: {stored_sampled_value} should either be a single-phase or triphase")
 
-        for phase in ["L2", "L3"]:
-            for unit, d in phase_values[phase].items():
-                # If it is not in L1, add it to L1
-                for phase_value in d:
-                    if not _same_type_sample_values(phase_value, merged_result["L1"][unit]):
-                        merged_result["L1"][unit].append(phase_value)
+    # Process L2 & L3 sampled values
+    supplements = {unit: [] for unit in valid_units}
+    for phase in ["L2", "L3"]:
+        for unit, d in phase_unit_sampled_values[phase].items():
+            # If it is not in L1, add it to L1
+            for stored_sampled_value in d:
+                if not _same_type_sample_values(stored_sampled_value, merged_result["L1"][unit]):
+                    is_having_same_type = _same_type_sample_values(stored_sampled_value, supplements[unit])
+                    supplements[unit].append(stored_sampled_value)
+                    if is_having_same_type:
+                        # Both L2 & L3 have the same type while L1 doesn't
+                        raise ValidationError(f"Invalid phase configuration! Should either be single-phase or triphase, now {supplements[unit]}")
+    for u, s in supplements.items():
+        merged_result["L1"][u].extend(s)
 
-
-        # Merge the phase values into the result
-        for phase in l_phases:
-            merged_result[phase] = [i for lst in merged_result[phase].values() for i in lst]
+    # Merge the phase values into the result
+    for phase in l_phases:
+        merged_result[phase] = [i for lst in merged_result[phase].values() for i in lst]
 
     r = ParserStepIO(content=[merged_result], request_type=data.request_type)
     return r
 
 CHARGER_REQUEST_PARSER_MAP = {
     'datatransfer': BaseParser(steps=[add_charger_number_and_raw_data_to_content]),
-    'metervalues': BaseParser(steps=[add_charger_number_and_raw_data_to_content, flatten_meter_value]),
+    'metervalues': BaseParser(steps=[add_charger_number_and_raw_data_to_content, flatten_meter_value, process_sampled_values]),
 }
 
 class ParserPattern(BaseModel):
